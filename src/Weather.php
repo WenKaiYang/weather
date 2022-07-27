cb09558e7727cda888471ec44958137081895ac4
织梦行云之唯心所识 <313097973@qq.com>
2022-07-27 15:05:21 +0800

    天气查询 单元测试

--- src/Weather.php
+++ src/Weather.php
@@ -32,16 +32,17 @@ class Weather
      * @param $city
      * @param string $type
      * @param string $format
-     * @return string
+     * @return array|string
      * @throws GuzzleException
      * @throws HttpException
      * @throws InvalidArgumentException
      */
-    public function getWeather($city, string $type = 'base', string $format = 'json'): string
+    public function getWeather($city, string $type = 'base', string $format = 'json'): array|string
     {
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
 
-        if (!\in_array(\strtolower($format), ['xml', 'json'])) {
+        // 1. 对 `$format` 与 `$type` 参数进行检查，不在范围内的抛出异常。
+        if (!\in_array($format, ['xml', 'json'])) {
             throw new InvalidArgumentException('Invalid response format: ' . $format);
         }
 
@@ -49,21 +50,30 @@ class Weather
             throw new InvalidArgumentException('Invalid type value(base/all): ' . $type);
         }
 
+        // 2. 封装 query 参数，并对空值进行过滤。
         $query = array_filter([
-                'key' => $this->key,
-                'city' => $city,
-                'output' => \strtolower($format),
-                'extensions' => \strtolower($type),
+            'key' => $this->key,
+            'city' => $city,
+            'output' => $format,
+            'extensions' => $type,
         ]);
 
         try {
+            // 3. 调用 getHttpClient 获取实例，并调用该实例的 `get` 方法，
+            // 传递参数为两个：$url、['query' => $query]，
             $response = $this->getHttpClient()
-                    ->get($url, ['query' => $query,])
-                    ->getBody()
-                    ->getContents();
+                ->get($url, [
+                    'query' => $query,
+                ])
+                ->getBody()
+                ->getContents();
 
-            return 'json' === $format ? \json_decode($response, true) : $response;
+            // 4. 返回值根据 $format 返回不同的格式，
+            // 当 $format 为 json 时，返回数组格式，否则为 xml。
+            return $format === 'json' ? \json_decode($response, true) : $response;
         } catch (\Exception $e) {
+            // 5. 当调用出现异常时捕获并抛出，消息为捕获到的异常消息，
+            // 并将调用异常作为 $previousException 传入。
             throw new HttpException($e->getMessage(), $e->getCode(), $e);
         }
     }
